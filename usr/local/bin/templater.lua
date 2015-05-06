7c5e206b52c3773489ee61227ec3d4f6cfab87b6
Daniel Podolsky <onokonem@gmail.com>
2015-05-06 22:53:25 +0300

    improvement: advanced version of templater

--- usr/local/bin/templater.lua
+++ usr/local/bin/templater.lua
@@ -2,4 +2,10 @@
 
 require "fwwrt.simplelp"
 
-print(fwwrt.simplelp.doString(fwwrt.simplelp.handlerToVariable(io.stdin), {}))
+local osEnv = {}
+for line in io.popen("set"):lines() do 
+  envName = line:match("^[^=]+")
+  osEnv[envName] = os.getenv(envName)
+end
+
+print(fwwrt.simplelp.doString(fwwrt.simplelp.handlerToVariable(io.stdin), osEnv))
